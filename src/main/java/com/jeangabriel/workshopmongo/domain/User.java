db178b32335e61e35b34b031681008f03f1fd3cd
Jean Gabriel <jeangabriel180@hotmail.com>
2022-04-15 11:49:03 -0300

    instanciacao base de dados

--- src/main/java/com/jeangabriel/workshopmongo/domain/User.java
+++ src/main/java/com/jeangabriel/workshopmongo/domain/User.java
@@ -5,6 +5,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 import java.util.Objects;
+
 @Document(collection = "user")
 public class User implements Serializable {
     @Id
