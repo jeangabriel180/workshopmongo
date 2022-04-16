07fadcc72c98af2afd7a6c6249c567e3055fedcf
Jean Gabriel <jeangabriel180@hotmail.com>
2022-04-16 13:19:15 -0300

    inserção de usuário com post

--- src/main/java/com/jeangabriel/workshopmongo/services/UserService.java
+++ src/main/java/com/jeangabriel/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.jeangabriel.workshopmongo.services;
 
 import com.jeangabriel.workshopmongo.domain.User;
+import com.jeangabriel.workshopmongo.dto.UserDTO;
 import com.jeangabriel.workshopmongo.repository.UserRepository;
 import com.jeangabriel.workshopmongo.services.exception.ObjectNotFoundExpection;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         return repository.findById(id)
                 .orElseThrow(() -> new ObjectNotFoundExpection("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repository.insert(obj);
+    }
+
+    public User fromDto(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
