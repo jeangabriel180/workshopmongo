a851e92e7c42e01f5d1c6890259d668dbf1270d7
Jean Gabriel <jeangabriel180@hotmail.com>
2022-04-17 18:32:39 -0300

    Referenciando posts do usuario

--- src/main/java/com/jeangabriel/workshopmongo/config/Instantion.java
+++ src/main/java/com/jeangabriel/workshopmongo/config/Instantion.java
@@ -42,5 +42,8 @@ public class Instantion implements CommandLineRunner {
                 "Acordei feliz hoje!", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
