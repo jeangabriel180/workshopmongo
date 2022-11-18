62f94e80fa30fe6bd3ebd89ee22c985daf94dcc9
Jean Gabriel <jeangabriel180@hotmail.com>
2022-11-18 10:04:51 -0300

    consulta simples com query methods

--- src/main/java/com/jeangabriel/workshopmongo/resources/PostResource.java
+++ src/main/java/com/jeangabriel/workshopmongo/resources/PostResource.java
@@ -3,6 +3,7 @@ package com.jeangabriel.workshopmongo.resources;
 import com.jeangabriel.workshopmongo.domain.Post;
 import com.jeangabriel.workshopmongo.domain.User;
 import com.jeangabriel.workshopmongo.dto.UserDTO;
+import com.jeangabriel.workshopmongo.resources.util.URL;
 import com.jeangabriel.workshopmongo.services.PostService;
 import com.jeangabriel.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,5 +28,11 @@ public class PostResource {
         return ResponseEntity.ok().body(post);
     }
 
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
 
 }
