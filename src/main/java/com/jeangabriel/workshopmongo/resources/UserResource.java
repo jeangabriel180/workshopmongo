2187f0301460fe41ef61252db219b02c80c0ff0b
Jean Gabriel <jeangabriel180@hotmail.com>
2022-11-18 09:34:03 -0300

    endpoint para retornar os posts de um usuário

--- src/main/java/com/jeangabriel/workshopmongo/resources/UserResource.java
+++ src/main/java/com/jeangabriel/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.jeangabriel.workshopmongo.resources;
 
+import com.jeangabriel.workshopmongo.domain.Post;
 import com.jeangabriel.workshopmongo.domain.User;
 import com.jeangabriel.workshopmongo.dto.UserDTO;
 import com.jeangabriel.workshopmongo.services.UserService;
@@ -32,6 +33,12 @@ public class UserResource {
     public ResponseEntity<UserDTO> findById(@PathVariable String id) {
         User user = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(user));
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(user.getPosts());
     }
 
     @PostMapping
